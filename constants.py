3dd9faa40ab5cc27e73884b4f6939350e49c21d6
HugoTHOLLON <hugo.thollon@gmail.com>
2025-11-07 18:42:33 +0100

    Algorithm finished. The algorithm is faster than the previous version but it needs to be optimized when dealing with a lot of data.

--- constants.py
+++ constants.py
@@ -1,15 +1,15 @@
 import pandas as pd
 
 # Constants
-NODES = "data_ariege/osm_nodes.csv"
-WAYS = "data_ariege/osm_ways.csv.gz"
+NODES = "data_serres/osm_nodes.csv"
+WAYS = "data_serres/osm_ways.csv"
 
 ### Serre test data
 STARTING_NODE_ID = 469819297  # Saint-Pierre-de-Rivière
 # STARTING_NODE_ID = 1469646001  # Cos
-# DESTINATION_NODE_ID = 1792742726  # Las Prados
+DESTINATION_NODE_ID = 1792742726  # Las Prados
 # DESTINATION_NODE_ID = 1205464576  # Cabane Coumauzil
-DESTINATION_NODE_ID = 1467560343  # Cautirac
+# DESTINATION_NODE_ID = 1467560343  # Cautirac
 
 ### Ariege test data
 # STARTING_NODE_ID = 27789470  # Rochers de la Mirouge
--- new_a_star.py
+++ new_a_star.py
@@ -1,6 +1,9 @@
+import cProfile
 import csv
 import gzip
 import heapq
+import io
+import pstats
 
 from haversine import haversine, Unit  # type: ignore
 
@@ -34,11 +37,10 @@ class AStarScore:
 
 
 class Node:
-    a_star_score: AStarScore
-
-    def __init__(self, lat: float, lon: float):
+    def __init__(self, lat: float, lon: float, a_star_score: AStarScore | None = None):
         self.lat = lat
         self.lon = lon
+        self.a_star_score = a_star_score
 
 
 class Way:
@@ -53,11 +55,11 @@ class Way:
 #########################################################################################
 #########################################################################################
 
-NODES = "data_ariege/osm_nodes.csv"
-WAYS = "data_ariege/osm_ways.csv.gz"
+NODES = "data_serres/osm_nodes.csv"
+WAYS = "data_serres/osm_ways.csv"
 
-START_ID: int = 1
-DESTINATION_ID: int = 1
+START_ID: int = 469819297
+DESTINATION_ID: int = 1792742726
 
 nodes: dict[int, Node] = {}
 ways: dict[int, list[Way]] = {}
@@ -71,10 +73,10 @@ def fill_nodes(reader: csv.DictReader):
     global nodes
     for row in reader:
         node_id = int(row["id"])
-        lon = float(row["lon"])
         lat = float(row["lat"])
+        lon = float(row["lon"])
 
-        nodes[node_id] = Node(lon, lat)
+        nodes[node_id] = Node(lat, lon)
 
 
 def fill_ways(reader: csv.DictReader):
@@ -89,6 +91,7 @@ def fill_ways(reader: csv.DictReader):
         ways.setdefault(node_to, []).append(Way(node_from, distance))
 
 
+print("Placing nodes in dict")
 # Read the osm_nodes.csv file and place it's data into a dict
 if NODES.endswith(".gz"):
     with gzip.open(NODES, mode="rt", newline="") as f:
@@ -98,6 +101,8 @@ else:
     with open(NODES, newline="") as f:
         fill_nodes(csv.DictReader(f))
 
+print("Nodes placed")
+print("Placing ways in dict")
 
 # Read the osm_ways.csv file and place it's data into a dict
 if WAYS.endswith(".gz"):
@@ -107,6 +112,7 @@ else:
     with open(WAYS, newline="") as f:
         fill_ways(csv.DictReader(f))
 
+print("Ways placed")
 
 #########################################################################################
 #########################################################################################
@@ -132,8 +138,8 @@ def get_haversine_distance(node1_id, node2_id):
     # else the distance wasn't computed yet
     else:
         # Compute the distance using the haversine formula
-        node1 = tuple(nodes[node1_id].lat, nodes[node1_id].lon)
-        node2 = tuple(nodes[node2_id].lat, nodes[node2_id].lon)
+        node1 = (nodes[node1_id].lat, nodes[node1_id].lon)
+        node2 = (nodes[node2_id].lat, nodes[node2_id].lon)
         heuristics[(node1_id, node2_id)] = haversine(node1, node2, unit=Unit.KILOMETERS)
     # return the distance
     return heuristics.get((node1_id, node2_id))
@@ -199,20 +205,56 @@ def a_star() -> dict | None:
         chosen_nodes[smallest_score.node_id] = smallest_score
         chosen_node_id = smallest_score.node_id
 
-    # TODO end of the algorithm
-    # # end of the algorithm, the shortest path has been found
-    # # format the returned data
-    # path = {
-    #     "distance": a_star_dict[chosen_node_id].weight,
-    #     "node_path": [chosen_node_id],
-    # }
-    # # get all parents using parent_node_id like a linked list starting with the last node.
-    # parent = a_star_dict[chosen_node_id].parent_node_id
-    # while parent:
-    #     path["node_path"].append(parent)
-    #     parent = a_star_dict[parent].parent_node_id
-    # # set the node path like: [start_node, node_1, ..., node_n, end_node]
-    # path["node_path"].reverse()
+    # end of the algorithm, the shortest path has been found
+    # format the returned data
+    path: dict = {
+        "distance": chosen_nodes[chosen_node_id].weight,
+        "node_path": [
+            (chosen_node_id, chosen_nodes[chosen_node_id].weight),
+        ],
+    }
+    # get all parents using parent_node_id like a linked list starting with the last node.
+    node_score = nodes[chosen_node_id].a_star_score
+    parent: int | None = node_score.parent_node_id if node_score else None
+    while parent:
+        path["node_path"].append((parent, chosen_nodes[parent].weight))
+        node_score = nodes[parent].a_star_score
+        parent = node_score.parent_node_id if node_score else None
+    # set the node path like: [start_node, node_1, ..., node_n, end_node]
+    path["node_path"].reverse()
 
-    # return path
-    return {}
+    return path
+
+
+#########################################################################################
+#########################################################################################
+################################# Launching algorithme ##################################
+#########################################################################################
+#########################################################################################
+
+print(
+    "Straight line distance: %s km" % get_haversine_distance(DESTINATION_ID, START_ID)
+)
+print("Starting algorithm")
+print("------------------")
+
+profiler = cProfile.Profile()
+result = profiler.runcall(a_star)
+
+s = io.StringIO()
+ps = pstats.Stats(profiler, stream=s)
+ps.print_stats(1)
+
+print("\n" + s.getvalue().strip().split("\n")[0] + "\n")
+
+
+if result:
+    # Print straight line distance and the one found. The straight line should be a little shorter.
+    print("Distance: %s km" % result["distance"])
+    # Print the path
+    i = 0
+    for node in result["node_path"]:
+        print("Node %i: %i - %f km" % (i, node[0], node[1]))
+        i += 1
+else:
+    print("path not found")
